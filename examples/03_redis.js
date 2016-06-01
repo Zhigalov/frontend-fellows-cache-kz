2c9e4d849c4f19b42397cfe7f5b94b01723cbb0d
Sergey Zhigalov <zhigalov@yandex-team.ru>
2016-06-01 20:30:09 +0500

    Redis.io, попадание в кеш, разогрев

---
+++ examples/03_redis.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var fetchProducts = require('./fetchProducts');
+var redis = require('redis');
+var bluebird = require('bluebird');
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+
+var client = redis.createClient();
+
+app.get('/', (req, res) => {
+    client.getAsync('coffee')
+        .then(data => {
+            return JSON.parse(data) || fetchProducts();
+        })
+        .then(data => {
+            client.set('coffee', JSON.stringify(data));
+            return data;
+        })
+        .then(data => res.json(data));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
